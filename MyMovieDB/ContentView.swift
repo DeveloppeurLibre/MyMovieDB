0408e26c11837c3a8beaa16ad701b52afc7a0980
Quentin Cornu <contact@developpeurlibre.com>
2021-07-16 15:38:10 +0200

    Initial Commit

---
+++ MyMovieDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMovieDB
+//
+//  Created by Quentin Cornu on 16/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMovieDB/MyMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieDBApp.swift
+//  MyMovieDB
+//
+//  Created by Quentin Cornu on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
