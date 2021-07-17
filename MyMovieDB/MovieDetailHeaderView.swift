395ef2211dfd40b71a07083fb641efe91d569977
Quentin Cornu <contact@developpeurlibre.com>
2021-07-17 14:35:22 +0200

    Create MovieDetailHeaderView

---
+++ MyMovieDB/MovieDetailHeaderView.swift
@@ -0,0 +1,66 @@
+//
+//  MovieDetailHeaderView.swift
+//  MyMovieDB
+//
+//  Created by Quentin Cornu on 17/07/2021.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct MovieDetailHeaderView: View {
+	
+	let imageURL: URL?
+	let title: String
+	let seasonsCount: Int
+	let genres: [String]
+	let rating: Float
+	
+    var body: some View {
+		VStack {
+			ZStack(alignment: .bottom) {
+				KFImage(imageURL)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(height: 300)
+				HStack(alignment: .bottom) {
+					VStack(alignment: .leading, spacing: 8) {
+						Text(title)
+							.font(.title)
+							.fontWeight(.bold)
+							.foregroundColor(.white)
+						Text("\(seasonsCount) saisons" + " | " + genres.joined(separator: " • "))
+							.foregroundColor(Color.white.opacity(0.6))
+							.font(.footnote)
+					}
+					Spacer()
+					CircleProgressBar(progress: rating)
+						.frame(width: 45, height: 45)
+						.padding(2.5)
+				}
+				.padding()
+				.padding(.top, 80)
+				.background(
+					LinearGradient(
+						gradient: Gradient(colors: [.clear, Color(red: 22/255, green: 32/255, blue: 53/255)]),
+						startPoint: .top,
+						endPoint: .bottom
+					)
+				)
+			}
+		}
+    }
+}
+
+struct MovieDetailHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+		MovieDetailHeaderView(
+			imageURL: URL(string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"),
+			title: "Ragnarök",
+			seasonsCount: 2,
+			genres: ["Science-Fiction", "Fantastique", "Drame", "Mystère"],
+			rating: 8.1
+		)
+		.previewLayout(.sizeThatFits)
+    }
+}
