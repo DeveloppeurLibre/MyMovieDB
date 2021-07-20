3c3c923ee5cad75e8751054b8110a73f8bbd65c0
Quentin Cornu <contact@developpeurlibre.com>
2021-07-20 11:02:24 +0200

    Convert Data in JSON

--- MyMovieDB/RealMovieRepository.swift
+++ MyMovieDB/RealMovieRepository.swift
@@ -14,10 +14,27 @@ class RealMovieRepository {
 		
 		let task = URLSession.shared.dataTask(with: url) { data, response, error in
 			guard let data = data else { return }
-			print(String(data: data, encoding: .utf8)!)
+			guard let json = self.getJSON(from: data) else { return }
+			
+			let results = json["results"] as! [[String: Any]]
+			
+			for result in results {
+				let id = result["id"] as! Int
+				print(id)
+			}
 		}
 		task.resume()
 		
 		return []
+	}
+	
+	private func getJSON(from data: Data) -> [String: Any]? {
+		do {
+			let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+			return json
+		} catch {
+			print("Erreur de conversion")
+			return nil
+		}
 	}
 }
