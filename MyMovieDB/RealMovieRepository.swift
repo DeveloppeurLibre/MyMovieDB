0fa7cc833797c898c472fe569b6db41e0cce23ae
Quentin Cornu <contact@developpeurlibre.com>
2021-08-11 13:56:09 +0200

    Get casting details

--- MyMovieDB/RealMovieRepository.swift
+++ MyMovieDB/RealMovieRepository.swift
@@ -28,6 +28,82 @@ class RealMovieRepository {
 		task.resume()
 	}
 	
+	func getMovieDetails(id: Int, casting: [Actor], completion: @escaping (Serie) -> Void) {
+		let url = URL(string: "https://api.themoviedb.org/3/tv/\(id)?api_key=\(API_KEY)&language=fr")!
+		
+		let task = URLSession.shared.dataTask(with: url) { data, response, error in
+			guard let data = data else { return }
+			guard let json = self.getJSON(from: data) else { return }
+			
+			let posterPath = json["poster_path"] as! String
+			let name = json["name"] as! String
+			let overview = json["overview"] as! String
+			let voteAverage = json["vote_average"] as! NSNumber
+			let genresJSON = json["genres"] as! [[String: Any]]
+			
+			// Récupérer les genres
+			var genres = [String]()
+			for genre in genresJSON {
+				let newGenre = genre["name"] as! String
+				genres.append(newGenre)
+			}
+			
+			// Récupérer les saisons
+			let seasonJSON = json["seasons"] as! [[String: Any]]
+			var seasons = [Season]()
+			for season in seasonJSON {
+				let newSeason = Season(
+					imageURL: URL(string: "https://image.tmdb.org/t/p/w500/\(season["poster_path"] as! String)"),
+					seasonNumber: season["season_number"] as! Int,
+					episodesCound: season["episode_count"] as! Int,
+					description: season["overview"] as! String
+				)
+				seasons.append(newSeason)
+			}
+			
+			let serie = Serie(
+				imageURL: URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)"),
+				title: name,
+				description: overview,
+				seasons: seasons,
+				genres: genres,
+				casting: casting,
+				rating: Float(truncating: voteAverage)
+			)
+			
+			completion(serie)
+		}
+		task.resume()
+	}
+	
+	func getCreditsDetails(forMovieID id: Int, completion: @escaping ([Actor]) -> Void) {
+		let url = URL(string: "https://api.themoviedb.org/3/tv/\(id)/credits?api_key=\(API_KEY)&language=fr")!
+		
+		let task = URLSession.shared.dataTask(with: url) { data, response, error in
+			guard let data = data else { return }
+			guard let json = self.getJSON(from: data) else { return }
+			
+			var actors = [Actor]()
+			let cast = json["cast"] as! [[String: Any]]
+			for actor in cast {
+				let profilePath = actor["profile_path"] as! String
+				let name = actor["name"] as! String
+				let character = actor["character"] as! String
+				
+				let newActor = Actor(
+					url: URL(string: "https://image.tmdb.org/t/p/w500/\(profilePath)"),
+					name: name,
+					characterName: character
+				)
+				actors.append(newActor)
+			}
+			
+			completion(actors)
+		}
+		
+		task.resume()
+	}
+	
 	private func getJSON(from data: Data) -> [String: Any]? {
 		do {
 			let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
